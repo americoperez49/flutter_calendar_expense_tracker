4fb8bc9fa5c2bed90a79fa925b146f1964a4333f
Americo Perez <americoperez49@gmail.com>
2022-06-23 00:49:27 -0500

    Now handling state with Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,131 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_expense_tracker/pages/calendar.dart';
+import 'package:flutter_calendar_expense_tracker/pages/calendar2.dart';
 import 'package:flutter_calendar_expense_tracker/pages/login.dart';
+import 'package:flutter_calendar_expense_tracker/pages/login2.dart';
+import 'package:flutter_calendar_expense_tracker/providers/googler_signin_provider.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-
-GoogleSignIn _googleSignIn = GoogleSignIn(
-  // Optional clientId
-  // clientId:
-  //     '855883079806-f605ka3euedgff42kd9tffmk980l73ok.apps.googleusercontent.com',
-  scopes: <String>[
-    'email',
-    'https://www.googleapis.com/auth/calendar',
-  ],
-);
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // return const MaterialApp(
+    //   title: 'Google Sign In',
+    //   home: LoginScreen(),
+    // );
+    // return MultiProvider(
+    //   providers: [
+    //     StreamProvider<GoogleSignInAccount?>.value(
+    //       value: _googleSignIn.onCurrentUserChanged,
+    //       initialData: null,
+    //     )
+    //   ],
+    //   child: const MaterialApp(
+    //     title: 'Google Sign In',
+    //     home: LoginScreen2(),
+    //   ),
+    // );
+    return MaterialApp(
       title: 'Google Sign In',
-      home: LoginScreen(),
-    ),
-  );
+      home: ChangeNotifierProvider(
+        create: ((context) => GoogleSignInProvider()),
+        child: MaterialApp(
+          title: 'Google Sign In',
+          home: LoginScreen2(),
+        ),
+      ),
+    );
+  }
 }
 
 //Login Page
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+// class LoginScreen extends StatefulWidget {
+//   const LoginScreen({super.key});
 
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
+//   @override
+//   State<LoginScreen> createState() => _LoginScreenState();
+// }
 
-class _LoginScreenState extends State<LoginScreen> {
-  GoogleSignInAccount? _currentUser;
+// class _LoginScreenState extends State<LoginScreen> {
+//   GoogleSignInAccount? _currentUser;
 
-  @override
-  void initState() {
-    super.initState();
-    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
-      setState(() {
-        _currentUser = account;
-      });
+//   @override
+//   void initState() {
+//     super.initState();
+//     _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+//       setState(() {
+//         _currentUser = account;
+//       });
 
-      if (_currentUser != null) {
-        //update the top bar
-        // _handleGetCalenderEvents();
-        print(_currentUser);
-      }
-    });
-    _googleSignIn.signInSilently();
-  }
+//       if (_currentUser != null) {
+//         //update the top bar
+//         // _handleGetCalenderEvents();
+//         print(_currentUser);
+//       }
+//     });
+//     _googleSignIn.signInSilently();
+//   }
 
-  Widget _buildScreen() {
-    if (_currentUser != null) {
+//   Widget _buildScreen() {
+//     if (_currentUser != null) {
+//       //create the scaffold that has:
+//       //user profile in app bar
+//       //buttons & calendar events for the body
+//       return CalendarView(
+//         googleSignIn: _googleSignIn,
+//         photoURL: _currentUser!.photoUrl,
+//         userName: _currentUser!.displayName,
+//       );
+//     } else {
+//       //create the scaffold that has:
+//       //no user profile in app bar
+//       //login button
+//       return Login(_googleSignIn);
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return _buildScreen();
+//   }
+// }
+
+class LoginScreen2 extends StatelessWidget {
+  const LoginScreen2({Key? key}) : super(key: key);
+
+  Widget _buildScreen(GoogleSignInProvider googleSignInProvider) {
+    if (googleSignInProvider.user != null) {
       //create the scaffold that has:
       //user profile in app bar
       //buttons & calendar events for the body
-      return CalendarView(
-        googleSignIn: _googleSignIn,
-        photoURL: _currentUser!.photoUrl,
-        userName: _currentUser!.displayName,
-      );
+      // return CalendarView(
+      //   googleSignIn: _googleSignIn,
+      //   photoURL: _currentUser!.photoUrl,
+      //   userName: _currentUser!.displayName,
+      // );
+      return CalendarView2();
     } else {
       //create the scaffold that has:
       //no user profile in app bar
       //login button
-      return Login(_googleSignIn);
+      // return Login(_googleSignIn);
+      return Login2();
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return _buildScreen();
+    return Consumer<GoogleSignInProvider>(
+      builder: (context, googleSignInProvider, child) {
+        return _buildScreen(googleSignInProvider);
+      },
+    );
   }
 }
---
+++ lib/pages/calendar2.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calendar_expense_tracker/providers/googler_signin_provider.dart';
+import 'package:flutter_calendar_expense_tracker/widgets/number_input.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
+import 'package:rxdart/rxdart.dart';
+
+class CalendarView extends StatefulWidget {
+  final GoogleSignIn googleSignIn;
+  final String? photoURL;
+  final String? userName;
+  const CalendarView(
+      {Key? key, required this.googleSignIn, this.photoURL, this.userName})
+      : super(key: key);
+
+  @override
+  State<CalendarView> createState() => _CalendarViewState();
+}
+
+class _CalendarViewState extends State<CalendarView> {
+  bool _moneyButtonEnabled = false;
+  BehaviorSubject<bool> $moneyButton = BehaviorSubject<bool>();
+  late MyNumberInput input;
+
+  @override
+  void initState() {
+    super.initState();
+    input = MyNumberInput(
+      myLabel: 'Weeks to Calculate',
+      myIcon: Icons.calendar_month_rounded,
+      enabled: true,
+      $moneyButton: $moneyButton,
+    );
+
+    $moneyButton.listen((value) => {
+          setState(() {
+            _moneyButtonEnabled = value;
+          })
+        });
+  }
+
+  Future<void> _handleSignOut() async {
+    await widget.googleSignIn.disconnect();
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.amber,
+      actions: [
+        IconButton(icon: const Icon(Icons.logout), onPressed: _handleSignOut),
+      ],
+      title: Row(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(80),
+          child: Image.network(
+            widget.photoURL!,
+            height: 35,
+          ),
+        ),
+        const SizedBox(
+          width: 5,
+        ),
+        Text(
+          widget.userName!,
+          style: const TextStyle(fontSize: 20),
+        )
+      ]),
+    );
+  }
+
+  Widget _buildBody() {
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.amber[700],
+      child: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          input,
+          const SizedBox(height: 6),
+          MyNumberInput(
+            myLabel: 'Money in the Bank',
+            myIcon: Icons.attach_money,
+            enabled: _moneyButtonEnabled,
+          ),
+          ElevatedButton(onPressed: () => {}, child: const Text("CLick me"))
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: _buildAppBar(), body: _buildBody());
+  }
+}
+
+class CalendarView2 extends StatelessWidget {
+  CalendarView2({Key? key}) : super(key: key);
+  MyNumberInput input = MyNumberInput(
+    myLabel: 'Weeks to Calculate',
+    myIcon: Icons.calendar_month_rounded,
+    enabled: true,
+    $moneyButton: BehaviorSubject<bool>(),
+  );
+
+  AppBar _buildAppBar(context) {
+    return AppBar(
+      backgroundColor: Colors.amber,
+      actions: [
+        Consumer<GoogleSignInProvider>(
+            builder: (context, googleSignInProvider, child) => IconButton(
+                icon: const Icon(Icons.logout),
+                onPressed: () => googleSignInProvider.signOut())),
+      ],
+      title: Consumer<GoogleSignInProvider>(
+        builder: (context, googleSignInProvider, child) => Row(children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(80),
+            child: Image.network(
+              googleSignInProvider.user!.photoUrl!,
+              height: 35,
+            ),
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          Text(
+            googleSignInProvider.user!.displayName!,
+            style: const TextStyle(fontSize: 20),
+          )
+        ]),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.amber[700],
+      child: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          input,
+          const SizedBox(height: 6),
+          MyNumberInput(
+            myLabel: 'Money in the Bank',
+            myIcon: Icons.attach_money,
+            // enabled: _moneyButtonEnabled,
+            enabled: true,
+          ),
+          ElevatedButton(onPressed: () => {}, child: const Text("CLick me"))
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: _buildAppBar(context), body: _buildBody());
+  }
+}
